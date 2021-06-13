d9ed2e5403aa2647840b833a17af48913cd0deb3
Paulo Alvim <pauloalvim@gmail.com>
2021-06-13 17:59:25 -0300

    Shows stats and abilities on PokemonScreen.dart.

--- lib/screens/Pokemon/PokemonScreen.dart
+++ lib/screens/Pokemon/PokemonScreen.dart
@@ -1,3 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:livefarm_flutter_test/models/PokemonModel.dart';
 import 'package:livefarm_flutter_test/services/Extensions/stringCapitalize.dart';
@@ -29,8 +31,45 @@ class _PokemonScreenState extends State<PokemonScreen> {
       appBar: AppBar(
         title: Text(widget.pokemon.name.inCaps),
       ),
-      body: Center(
-        child: Text("Informações do pokemon"),
+      body: ListView(
+        children: [
+          CachedNetworkImage(
+              placeholder: (context, url) => CircularProgressIndicator(),
+              imageUrl: widget.pokemon.frontHighQuality),
+          Center(
+            child: Text("Abilities"),
+          ),
+          Container(
+            child: CarouselSlider(
+              items: widget.pokemon.abilities
+                  .map((ability) => Center(
+                        child: Text(
+                          "${ability.name} : ${ability.slot}",
+                          textAlign: TextAlign.center,
+                        ),
+                      ))
+                  .toList(),
+              options: CarouselOptions(height: 50),
+            ),
+          ),
+          Center(child: Text("Stats")),
+          Container(
+            margin: EdgeInsets.only(left: 20, right: 20),
+            child: GridView.count(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              crossAxisCount: 3,
+              childAspectRatio: 2,
+              children: widget.pokemon.stats
+                  .map((stat) => Center(
+                          child: Text(
+                        "${stat.name.allInCaps} : ${stat.baseStat}",
+                        textAlign: TextAlign.center,
+                      )))
+                  .toList(),
+            ),
+          )
+        ],
       ),
     ));
   }
